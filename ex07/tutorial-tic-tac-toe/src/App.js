a531b363ca3cbb115137d3d06f889b434ead85e6
Dandy Now! <75767073+postforty@users.noreply.github.com>
2024-10-01 23:22:27 +0900

    Update App.js

--- ex07/tutorial-tic-tac-toe/src/App.js
+++ ex07/tutorial-tic-tac-toe/src/App.js
@@ -9,30 +9,30 @@ function calculateWinner(squares) {
     [1, 4, 7],
     [2, 5, 8],
     [0, 4, 8],
-    [2, 4, 6]
-  ]
-  // for(let i=0; i<lines.length; i++) {
-  //   const [a,b,c] = lines[i];
-  //   if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-  //     return squares[a];
-  //   }
-  // }
-  // return null;
+    [2, 4, 6],
+  ];
 
+  const result = lines.find(
+    ([a, b, c]) =>
+      squares[a] && squares[a] === squares[b] && squares[a] === squares[c]
+  );
 
-  const result = squares.find(square => lines.some(([a, b, c]) =>
-    squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) ? square : null);
+  console.log(result);
 
-  return result;
+  return result ? squares[result[0]] : null;
 }
 
 function Square({ value, onSquareClick }) {
-  return <button className="square" onClick={onSquareClick}>{value}</button>
+  return (
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  );
 }
 
 export default function Board() {
   // const [squares, setSquares] = useState(Array.from({length:9}, ()=> null))
-  const [squares, setSquares] = useState(Array(9).fill(null))
+  const [squares, setSquares] = useState(Array(9).fill(null));
   const [xIsNext, setXIsNext] = useState(true);
 
   function handleClick(i) {
@@ -47,7 +47,7 @@ export default function Board() {
     }
     setSquares(nextSquares);
     setXIsNext(!xIsNext);
-    console.log(nextSquares)
+    console.log(nextSquares);
   }
 
   const winner = calculateWinner(squares);
@@ -78,5 +78,5 @@ export default function Board() {
         <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
       </div>
     </>
-  )
+  );
 }
